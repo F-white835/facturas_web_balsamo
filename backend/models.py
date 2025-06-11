294594b595dfd57756458a4a09df80fc49863892
F-white835 <escobar.2271@gmail.com>
2025-06-11 16:03:15 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,8 @@
-# backend/models.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
 class Factura(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cliente = db.Column(db.String(100), nullable=False)
     monto = db.Column(db.Float, nullable=False)
-    fecha = db.Column(db.DateTime, nullable=False)
+    fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    tipo_comprobante = db.Column(db.String(50))      # Nuevo
+    forma_pago = db.Column(db.String(50))            # Nuevo
+    nit = db.Column(db.String(50))                   # Nuevo
