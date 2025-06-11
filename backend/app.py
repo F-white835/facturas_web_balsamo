629215180521b961d752352f9f55af9ecab228e4
felix <escobar.2271@gmail.com>
2025-06-11 11:22:19 -0600

    Actualizar app.py para usar DATABASE_URL en Render

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
+with app.app_context():
+    db.create_all()  # <- crea las tablas si no existen
 
 class FacturaForm(FlaskForm):
     cliente = StringField('Cliente', validators=[DataRequired()])
