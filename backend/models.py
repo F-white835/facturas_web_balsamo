4418e20254d12eb764735f1931fcc7dec2e19ac4
F-white835 <escobar.2271@gmail.com>
2025-06-11 16:32:02 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,12 @@
+from backend.extensions import db
+from datetime import datetime
+
 class Factura(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cliente = db.Column(db.String(100), nullable=False)
     monto = db.Column(db.Float, nullable=False)
     fecha = db.Column(db.DateTime, default=datetime.utcnow)
-    tipo_comprobante = db.Column(db.String(50))      # Nuevo
-    forma_pago = db.Column(db.String(50))            # Nuevo
-    nit = db.Column(db.String(50))                   # Nuevo
+    tipo_comprobante = db.Column(db.String(50))
+    forma_pago = db.Column(db.String(50))
+    nit = db.Column(db.String(20))
+
